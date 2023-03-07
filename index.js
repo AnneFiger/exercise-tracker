b3aa680cba97dee461e4a3de08ecb784e3f91dfe
AnneFiger <annefiger@gmail.com>
2023-03-07 20:05:11 +0000

    Fork boilerplate repo, connect DB, and set up first post request and response include user model for db and using urlencoded for post request

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express()
-const cors = require('cors')
-require('dotenv').config()
+
+
+
+mongoose
+  .connect('mongodb+srv://annefiger:pChsE2BoyqEy8GXv@exercise-tracker.olxjy6t.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+.then(() => {
+  console.log('Mongodb connected');
+});
+
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+
+
+// You can POST to /api/users with form data username to create a new user.
+app.post("/api/users", function (req, res) {
+  const user = new User({
+    username : req.body.username
+  });
+  user.save().then((result) => {
+     res.send(
+      result   // see if need to get rid of v_
+     );
+   })
+});
 
 
 
